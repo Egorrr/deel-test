443149697f53034bf85f20221f815d232a2598b7
Egor Yatsuk <egorrr.yatsuk@gmail.com>
2022-03-16 22:35:52 +0100

    app: add response time middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const responseTime = require('response-time')
 const controllers = require('./controllers');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+app.use(responseTime());
 app.use('/api', controllers);
 app.use(errorHandler());
 
